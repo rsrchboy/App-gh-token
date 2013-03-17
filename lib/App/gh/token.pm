0268bb83a23dd15be0b793cf9b5690dffcfac358
Chris Weyl <cweyl@alumni.drew.edu>
2013-03-16 20:39:43 -0700

    initial commit

---
+++ lib/App/gh/token.pm
@@ -0,0 +1,19 @@
+package App::gh::token;
+
+# ABSTRACT: The great new App::gh::token!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
